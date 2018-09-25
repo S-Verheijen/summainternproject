51b776d37d65149a92c7803f082066156dbb1f44
dogus75 <ps174039@summacollege.nl>
2018-09-25 14:25:46 +0200

    menu veranderingen

--- menubar.php
+++ menubar.php
@@ -1,12 +1,11 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">
 
-<div id="mySidenav" class="sidenav">
-  <a class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#">Dashboard</a>
+<div id="mySidenav" onmouseleave="closeNav()" class="sidenav">
+  <a href="dashboard.php">Dashboard</a>
   <a href="#">Bestellingen</a>
   <a href="#">Agenda</a>
   <a href= "logout.php">Uitloggen</a>
 </div>
-<span id="menuIcon" onclick="openNav()">&#9776;</span>
+<span id="menuIcon"  onmouseenter="openNav()">&#9776;</span>
 
 <script src="javascript/menu.js"></script>
