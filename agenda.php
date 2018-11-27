f0d1038d7764c1cabcf4247904138ac9050f6fc6
dogus75 <ps174039@summacollege.nl>
2018-11-27 14:03:45 +0100

    agenda (beta)

--- agenda.php
+++ agenda.php
@@ -1,8 +1,99 @@
+<?php
+// Set your timezone
+date_default_timezone_set('Asia/Tokyo');
+// Get prev & next month
+if (isset($_GET['ym'])) {
+    $ym = $_GET['ym'];
+} else {
+    // This month
+    $ym = date('Y-m');
+}
+// Check format
+$timestamp = strtotime($ym . '-01');
+if ($timestamp === false) {
+    $ym = date('Y-m');
+    $timestamp = strtotime($ym . '-01');
+}
+// Today
+$today = date('Y-m-j', time());
+// For H3 title
+$html_title = date('Y / m', $timestamp);
+// Create prev & next month link     mktime(hour,minute,second,month,day,year)
+$prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
+$next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
+// You can also use strtotime!
+// $prev = date('Y-m', strtotime('-1 month', $timestamp));
+// $next = date('Y-m', strtotime('+1 month', $timestamp));
+// Number of days in the month
+$day_count = date('t', $timestamp);
+ 
+// 0:Sun 1:Mon 2:Tue ...
+$str = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
+//$str = date('w', $timestamp);
+// Create Calendar!!
+$weeks = array();
+$week = '';
+// Add empty cell
+$week .= str_repeat('<td></td>', $str);
+for ( $day = 1; $day <= $day_count; $day++, $str++) {
+     
+    $date = $ym . '-' . $day;
+     
+    if ($today == $date) {
+        $week .= '<td class="today">' . $day;
+    } else {
+        $week .= '<td>' . $day;
+    }
+    $week .= '</td>';
+     
+    // End of the week OR End of the month
+    if ($str % 7 == 6 || $day == $day_count) {
+        if ($day == $day_count) {
+            // Add empty cell
+            $week .= str_repeat('<td></td>', 6 - ($str % 7));
+        }
+        $weeks[] = '<tr>' . $week . '</tr>';
+        // Prepare for new week
+        $week = '';
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Dashboard</title>
   <link rel="stylesheet" type="text/css" href="css/style.css">
+  <style>
+        .container {
+            font-family: 'Noto Sans', sans-serif;
+            margin-top: 80px;
+        }
+        h3 {
+            margin-bottom: 30px;
+        }
+        th {
+            height: 30px;
+            text-align: center;
+        }
+        td {
+            height: 100px;
+        }
+        .today {
+            background: orange;
+        }
+        th:nth-of-type(1), td:nth-of-type(1) {
+            color: red;
+        }
+        th:nth-of-type(7), td:nth-of-type(7) {
+            color: blue;
+        }
+        wrapper
+        {
+          margin: 5%;
+          min-width: 200px;
+        }
+    </style>
 </head>
 <body>
   <div class="container">
@@ -11,10 +102,25 @@
       <h1>Dashboard</h1>
     </div>
     <div class="content">
-      <p>
-      
-      </p>
-      <h1 style="text-align: center; color: red">!!!UNDER CONSTRUCTION!!!</h1>
+    <div id="wrapper">
+    <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
+        <table class="table table-bordered">
+            <tr>
+                <th>S</th>
+                <th>M</th>
+                <th>T</th>
+                <th>W</th>
+                <th>T</th>
+                <th>F</th>
+                <th>S</th>
+            </tr>
+            <?php
+                foreach ($weeks as $week) {
+                    echo $week;
+                }
+            ?>
+        </table>
+        </div>
     </div>
     <div class="footer" style = "clear:both; text-align: center">
       <p style="padding-left: 10%; padding-right: 10%">Copyright © 2018 | Site uitsluitend bedoeld voor Summa ICT te Eindhoven</p>
@@ -22,4 +128,3 @@
   </div>
 </body>
 </html>
-
